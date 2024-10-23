b4d5056102e26a59c3ab139d76757fd4c3553332
fire0607 <stu00634@gmail.com>
2024-10-24 01:32:06 +0800

    feat:安裝nuxt ui

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,13 @@
   <p>若未登入，轉跳至登入頁面</p>
   <NuxtLink to="/logIn">登入</NuxtLink><br />
   <NuxtLink to="/signUp">註冊</NuxtLink>
+  <ULink
+    to="/signUp"
+    active-class="text-primary"
+    inactive-class="text-gray-500 dark:text-gray-400 hover:text-blue-300 dark:hover:text-gray-200"
+  >
+    測試元件
+  </ULink>
 </template>
 
 <style scoped></style>
